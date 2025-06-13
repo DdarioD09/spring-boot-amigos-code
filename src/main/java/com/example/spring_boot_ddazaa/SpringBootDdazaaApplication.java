e1b83718ecebb8a365fcefe64ea03b7ddec146ee
ddariod <dariod.dazaa09@gmail.com>
2025-06-13 10:52:29 -0500

    getting starter spring boot app

---
+++ src/main/java/com/example/spring_boot_ddazaa/SpringBootDdazaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_ddazaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDdazaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDdazaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_ddazaa/SpringBootDdazaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_ddazaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDdazaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
